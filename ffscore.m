d2bfde1dbbc6fbf75d84c8a644e573053f189c22
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2023-06-23 23:41:51 +0330

    Create ffscore.m

---
+++ ffscore.m
@@ -0,0 +1,16 @@
+function score=ffscore(R,m,n)
+%ffscore is fermatean fuzzy score function
+% r is decission cell
+% score is the fermatean fuzzy score
+%m is number of alternatives
+%n is number of criteria
+
+score=zeros(m,n); %empty score matrix
+    for i=1:m
+        for j=1:n
+                a=R{i,j};     % a is a dummy matrix 
+                score(i,j)=a(1)^3 - a(2)^3; %calculating score of R
+        end
+    end
+
+end

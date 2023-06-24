93c21f4ca92b76ecc0c26684ae63d240f44570f7
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2023-06-24 21:27:39 +0330

    Create ffdistance.m

---
+++ ffdistance.m
@@ -0,0 +1,20 @@
+function [Dplus,Dminus]=ffdistance(R,Splus,Sminus)
+
+[m,n]=size(R);
+Dplus=zeros(m,1);
+Dminus=zeros(m,1);
+d=zeros(2,n);
+    for i=1:m
+            for j=1:n
+                    a=R{i,j};
+                    b=Splus{1,j};
+                    c=Sminus{1,j};
+                    d(1,j)=w(j)*sqrt(.5*sum(((a.^3-b.^3).^2)));
+                    d(2,j)=w(j)*sqrt(.5*sum(((a.^3-c.^3).^2)));
+            end
+        Dplus(i)=sum(d(1,:));
+        Dminus(i)=sum(d(2,:));
+        d=zeros(2,n);
+    end
+
+end

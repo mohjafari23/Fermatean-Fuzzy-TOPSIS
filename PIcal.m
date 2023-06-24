4c4935a538c3d682335e3e2ffb576e464c43cd0c
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2023-06-24 21:09:47 +0330

    Update PIcal.m

--- PIcal.m
+++ PIcal.m
@@ -1,11 +1,12 @@
 function sp=PIcal(cell)
-%PI is the dgree of indeterminancy 
+%PI is the dgree of indeterminacy 
 %sp is the cell with the pi
 [m,n]=size(cell);
+sp=cell(m,n); %empty cell to reallocate memory
     for i=1:m
         for j=1:n
             a=cell{i,j}; % a is a dummy matrix 
-            sp{i,j}=[cell{i,j},nthroot((1-a(1)^3-a(2)^3),3)];   %Pi cell
+            sp{i,j}=[cell{i,j},nthroot((1-a(1)^3-a(2)^3),3)];   %Pi cal
     
         end
     end
